e0549b012f818825d623fd8d3438c2046eab280d
BumgeunSong <isp1195@gmail.com>
2021-11-11 22:26:55 -0800

    Try optimization for problem#3

--- cocoa-friday-algorithm/cocoa-friday-algorithm/week2/두 개 뽑아서 더하기.swift
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/week2/두 개 뽑아서 더하기.swift
@@ -10,10 +10,18 @@ import Foundation
 func pick2Add(_ numbers:[Int]) -> [Int] {
     
     var sums: Set<Int> = []
+    var pastIndex: Array<Int> = []
+    
     for index1 in 0...numbers.count - 2 {
+        if pastIndex.contains(numbers[index1]) {
+            continue
+        }
+        
         for index2 in index1 + 1...numbers.count - 1 {
             sums.insert(numbers[index1] + numbers[index2])
         }
+        
+        pastIndex.append(numbers[index1])
     }
     let result = Array(sums).sorted()
     return result
