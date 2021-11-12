8684ce09057978f20a785c2bdeb2353264da6604
BumgeunSong <isp1195@gmail.com>
2021-11-11 22:22:55 -0800

    Add solution to problem#3

--- cocoa-friday-algorithm/cocoa-friday-algorithm/main.swift
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/main.swift
@@ -81,3 +81,10 @@ let board1 = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 let moves1 = [1,5,3,5,1,2,1,4]
 print(craneGame(board1, moves1))
 
+
+//MARK: - 두 개 뽑아서 더하기
+
+let numbers1 = [2,1,3,4,1]
+let numbers2 = [5,0,2,7]
+print(pick2Add(numbers1), "should be [2,3,4,5,6,7]")
+print(pick2Add(numbers2), "should be [2,5,7,9,12]")
---
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/week2/두 개 뽑아서 더하기.swift
@@ -0,0 +1,20 @@
+//
+//  pick2Add.swift
+//  cocoa-friday-algorithm
+//
+//  Created by Bumgeun Song on 2021/11/11.
+//
+
+import Foundation
+
+func pick2Add(_ numbers:[Int]) -> [Int] {
+    
+    var sums: Set<Int> = []
+    for index1 in 0...numbers.count - 2 {
+        for index2 in index1 + 1...numbers.count - 1 {
+            sums.insert(numbers[index1] + numbers[index2])
+        }
+    }
+    let result = Array(sums).sorted()
+    return result
+}
