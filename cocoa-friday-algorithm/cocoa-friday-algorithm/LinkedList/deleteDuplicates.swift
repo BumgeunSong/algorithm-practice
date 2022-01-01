7c20769aa61a8b8d9c181e3e7a9579190d542cef
BumgeunSong <isp1195@gmail.com>
2022-01-01 22:20:17 +0900

    Add  Remove Duplicates2

---
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/LinkedList/deleteDuplicates.swift
@@ -0,0 +1,12 @@
+//
+//  deleteDuplicates.swift
+//  cocoa-friday-algorithm
+//
+//  Created by Bumgeun Song on 2022/01/01.
+//
+
+import Foundation
+
+func deleteDuplicates2(_ head: ListNode?) -> ListNode? {
+    return head
+}
