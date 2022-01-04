15be8e27b0d223638cde7ebeed28602a5c1c2b42
BumgeunSong <isp1195@gmail.com>
2022-01-04 21:57:55 +0900

    Add MylinkedList

---
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/LinkedList/designLinkedList.swift
@@ -0,0 +1,45 @@
+//
+//  designLinkedList.swift
+//  cocoa-friday-algorithm
+//
+//  Created by Bumgeun Song on 2022/01/04.
+//
+
+import Foundation
+
+class MyLinkedList {
+
+    init() {
+        
+    }
+    
+    func get(_ index: Int) -> Int {
+        
+    }
+    
+    func addAtHead(_ val: Int) {
+        
+    }
+    
+    func addAtTail(_ val: Int) {
+        
+    }
+    
+    func addAtIndex(_ index: Int, _ val: Int) {
+        
+    }
+    
+    func deleteAtIndex(_ index: Int) {
+        
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * let obj = MyLinkedList()
+ * let ret_1: Int = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index, val)
+ * obj.deleteAtIndex(index)
+ */
