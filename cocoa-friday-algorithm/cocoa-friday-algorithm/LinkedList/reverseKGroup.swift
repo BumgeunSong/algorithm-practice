32b983a27b4f1d4e7ad84975725cb9ef56af588f
BumgeunSong <isp1195@gmail.com>
2022-01-07 21:11:39 +0900

    Complete reverseKGroup

--- cocoa-friday-algorithm/cocoa-friday-algorithm/LinkedList/reverseKGroup.swift
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/LinkedList/reverseKGroup.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+    if head == nil || head?.next == nil || k == 1 { return head }
     
+    let answer = ListNode(0)
+    var p1 = answer
+    var p2 = head
+    
+    while true {
+        var p3 = p2
+        for _ in 0..<k-1 {
+            p3 = p3?.next
+            if p3 == nil {
+                p1.next = p2
+                return answer.next
+            }
+        }
+        
+        p3 = p2?.next
+        
+        for _ in 0..<k {
+            p2?.next = p1.next
+            p1.next = p2
+            p2 = p3
+            p3 = p3?.next
+        }
+        
+        for _ in 0..<k {
+            p1 = p1.next!
+        }
+        
+    }
 }
