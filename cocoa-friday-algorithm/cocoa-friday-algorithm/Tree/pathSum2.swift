a48e594fa86b603266810c4b10b734bf4b4a791b
BumgeunSong <isp1195@gmail.com>
2022-01-13 23:59:42 +0900

    Complete pathSum2

--- cocoa-friday-algorithm/cocoa-friday-algorithm/Tree/pathSum2.swift
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/Tree/pathSum2.swift
@@ -7,21 +7,21 @@
 
 import Foundation
 
+
 func pathSum2(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
     var result = [[Int]]()
     
     func DFS(_ root: TreeNode?, _ targetSum: Int, _ path: [Int]) {
+        if root == nil { return }
+        
+        let currentPath = path + [root!.val]
         if root?.left == nil && root?.right == nil {
-            if targetSum == path.reduce(0, {$0 + $1}) { result.append(path) }
+            if targetSum == currentPath.reduce(0, {$0 + $1}) { result.append(currentPath) }
             return
-        } else {
-            
         }
-        DFS(root?.left, targetSum, path + [root!.val])
-        DFS(root?.right, targetSum, path + [root!.val])
-        }
+        DFS(root?.left, targetSum, currentPath)
+        DFS(root?.right, targetSum, currentPath)
     }
     DFS(root, targetSum, [])
-    
     return result
 }
