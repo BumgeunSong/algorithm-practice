932094005a182c97eb2ecccd7ee699a766934d6b
BumgeunSong <isp1195@gmail.com>
2022-02-10 23:24:36 +0900

    Create connect 2

---
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/BFS_DFS/PopulatingNextRightPointers.swift
@@ -0,0 +1,12 @@
+//
+//  PopulatingNextRightPointers.swift
+//  cocoa-friday-algorithm
+//
+//  Created by Bumgeun Song on 2022/02/10.
+//
+
+import Foundation
+
+func connect2(_ root: Node?) -> Node? {
+    
+}
