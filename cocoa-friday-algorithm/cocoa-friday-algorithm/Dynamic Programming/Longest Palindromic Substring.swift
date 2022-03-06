cedc438145002998cac744f2c786b4b4b0ef4669
BumgeunSong <isp1195@gmail.com>
2022-03-06 22:56:17 +0900

    Complete Longest Palindromic Substring

--- cocoa-friday-algorithm/cocoa-friday-algorithm/Dynamic Programming/Longest Palindromic Substring.swift
+++ cocoa-friday-algorithm/cocoa-friday-algorithm/Dynamic Programming/Longest Palindromic Substring.swift
@@ -8,86 +8,34 @@
 import Foundation
 
 func longestPalindrome2(_ s: String) -> String {
-//    let arr = Array(s)
-//    let strlen = arr.count
-//    var isPalindrome = Array(repeating: Array(repeating: true, count: strlen),
-//                             count: strlen)
-//
-    
-    let s = Array(s)
-    let n = s.count
-    var isPalindrome = Array(repeating: Array(repeating: true, count: n), count: n)
-    
-    
-    var minBegin = 0
-    var maxLen = 0
-    
-    for len in 1...n {
-        for begin in 0...n-len {
-            print(s[begin..<begin+len])
-            if len == 1 {
-                isPalindrome[begin][begin+len-1] = true
-            } else if s[begin] == s[begin+len-1] && isPalindrome[begin][begin+len-1] {
-                isPalindrome[begin][begin+len-1] = true
-            } else {
-                isPalindrome[begin][begin+len-1] = false
-                continue
-            }
-            if len > maxLen {
-                minBegin = begin
-                maxLen = len
-            }
-        }
-    }
-    return s[minBegin..<minBegin+maxLen].map { "\($0)" }.joined(separator: "")
-    }
-    
-//    }
-//
-//    var minStart = 0
-//    var maxEnd = 0
-//
-//    var start = 0
-//    var len = 0
-//    while len <= strlen {
-//        if len == 0 {
-//            isPalindrome[start][start+len] = true
-//        } else if arr[start] == arr[start+len] && isPalindrome[start+1][start+len-1] {
-//            isPalindrome[start][start+len] = true
-//            if len > (maxEnd - minStart) {
-//                minStart = start
-//                maxEnd = start+len
-//            }
-//        } else {
-//            isPalindrome[start][start+len] = false
-//        }
-//
-//        if start < strlen-1 {
-//            start += 1
-//        } else {
-//            len += 1
-//            start = 0
-//        }
-//
-//    }
-//    
-//    for start in 0..<len {
-//        for end in 0..<len where start <= end {
-//            
-//            if arr[start] == arr[end] && isPalindrome[start+1][end-1] {
-//                isPalindrome[start][end] = true
-//                if (end - start) > (maxEnd - minStart) {
-//                    minStart = start
-//                    maxEnd = end
-//                }
-//            } else {
-//                isPalindrome[start][end] = false
-//            }
-//        }
-//    }
-//    
-//
-//
-//    return ""
+   if s.count == 0 { return "" }
+   
+   let s = s.map { String($0) }
+   let n = s.count
+   var isPalindrome = Array(repeating: Array(repeating: false, count: n), count: n)
+   
+   var begin = 0
+   var end = 0
+   var longest = (begin: 0, end: 0)
+   
+   for i in 0..<n {
+       for j in i..<n {
+           begin = j-i
+           end = j
+           
+           if i == 0 {
+               isPalindrome[begin][end] = true
+               continue
+           }
+           
+           if (s[begin] == s[end]) && (isPalindrome[begin+1][end-1] || begin == end-1) {
+               isPalindrome[begin][end] = true
+               longest = (begin, end)
+           }
+           
+       }
+   }
+   return s[longest.begin...longest.end].joined(separator: "")
+}
 
 
