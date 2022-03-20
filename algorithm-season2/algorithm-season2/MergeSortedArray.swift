c9cba5298b75541bae725d28c2bf460b4c94a90a
BumgeunSong <isp1195@gmail.com>
2022-03-20 22:22:29 +0900

    Add MergeSortedArray

---
+++ algorithm-season2/algorithm-season2/MergeSortedArray.swift
@@ -0,0 +1,36 @@
+//
+//  MergeSortedArray.swift
+//  algorithm-season2
+//
+//  Created by Bumgeun Song on 2022/03/20.
+//
+
+import Foundation
+
+extension Array {
+    static func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        let nums3 = nums1.prefix(m)
+        
+        var p1 = 0
+        var p2 = 0
+        var p3 = 0
+        
+        while p1 < m + n {
+            if p3 >= m {
+                nums1[p1] = nums2[p2]
+                p2 += 1
+            } else if p2 >= n {
+                nums1[p1] = nums3[p3]
+                p3 += 1
+            } else if nums2[p2] <= nums3[p3] {
+                nums1[p1] = nums2[p2]
+                p2 += 1
+            } else {
+                nums1[p1] = nums3[p3]
+                p3 += 1
+            }
+            
+            p1 += 1
+        }
+    }
+}
--- algorithm-season2/algorithmTests/algorithmTests.swift
+++ algorithm-season2/algorithmTests/algorithmTests.swift
@@ -24,4 +24,22 @@ class algorithmTests: XCTestCase {
         XCTAssertEqual(Array.maxSubArray(nums), 23)
         
     }
+    
+    func testMergeSortedArray1() throws {
+        var nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        Array.merge(&nums1, m, nums2, n)
+        XCTAssertEqual(nums1, [1,2,2,3,5,6])
+    }
+    
+    func testMergeSortedArray2() throws {
+        var nums1 = [1], m = 1, nums2 = [Int](), n = 0
+        Array.merge(&nums1, m, nums2, n)
+        XCTAssertEqual(nums1, [1])
+    }
+    
+    func testMergeSortedArray3() throws {
+        var nums1 = [1], m = 1, nums2 = [Int](), n = 0
+        Array.merge(&nums1, m, nums2, n)
+        XCTAssertEqual(nums1, [1])
+    }
 }
