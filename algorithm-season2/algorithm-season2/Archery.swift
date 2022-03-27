d68d68f27526c0c7fe4e83829febaad2f3aca1aa
BumgeunSong <isp1195@gmail.com>
2022-03-27 23:20:26 +0900

    Add kakao archery

---
+++ algorithm-season2/algorithm-season2/Archery.swift
@@ -0,0 +1,14 @@
+//
+//  Archery.swift
+//  algorithm-season2
+//
+//  Created by Bumgeun Song on 2022/03/27.
+//  https://programmers.co.kr/learn/courses/30/lessons/92342
+
+import Foundation
+
+struct Kakao {
+    func winner(_ n: Int, _ info: [Int]) -> [Int] {
+        return []
+    }
+}
