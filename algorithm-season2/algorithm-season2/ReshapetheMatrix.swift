cd8347ec6c53af75b39ae5b16d5167b1020ec62d
BumgeunSong <isp1195@gmail.com>
2022-03-28 23:44:29 +0900

    Add matrixReshape

---
+++ algorithm-season2/algorithm-season2/ReshapetheMatrix.swift
@@ -0,0 +1,14 @@
+//
+//  ReshapetheMatrix.swift
+//  algorithm-season2
+//
+//  Created by Bumgeun Song on 2022/03/28.
+//
+
+import Foundation
+
+extension Arrays {
+    func matrixReshape(_ mat: [[Int]], _ r: Int, _ c: Int) -> [[Int]] {
+        return [[Int]]()
+    }
+}
