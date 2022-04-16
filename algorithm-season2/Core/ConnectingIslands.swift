0c855ce18dceb168a2ce0a097e65c287bab8dd78
BumgeunSong <isp1195@gmail.com>
2022-04-16 22:44:14 +0900

    Complete connectIslands using non-mutating methods

--- algorithm-season2/Core/ConnectingIslands.swift
+++ algorithm-season2/Core/ConnectingIslands.swift
@@ -9,22 +9,34 @@ import Foundation
 
 func connectIslands(_ n: Int, _ costs: [[Int]]) -> Int {
     
-    let costsAscending = costs.sorted { $0[2] < $1[2] }
+    func findParent(_ parent: [Int], _ x: Int) -> Int {
+        return parent[x] == x ? parent[x] : findParent(parent, parent[x])
+    }
+    
+    func unionParent(_ parent: [Int], _ a: Int, _ b: Int) -> [Int] {
+        let rootA = findParent(parent, a)
+        let rootB = findParent(parent, b)
+
+        if rootA < rootB {
+            return parent.map { $0 == rootA ? rootB: $0 }
+        } else {
+            return parent.map { $0 == rootB ? rootA: $0 }
+        }
+    }
+    
+    func isParentSame(_ parent: [Int], _ a: Int, _ b: Int) -> Bool {
+        return findParent(parent, a) == findParent(parent, b)
+    }
     
     var parent = Array(0..<n)
-    var costSum = 0
-    for cost in costsAscending {
-        if getParent(cost[0]) == getParent(cost[1]) { continue }
-        costSum += cost[2]
-        let newParent = min(parent[cost[0]], parent[cost[1]])
-        parent[cost[0]] = newParent
-        parent[cost[1]] = newParent
+    let sortedCosts = costs.sorted { $0[2] < $1[2] }
+
+    return sortedCosts.reduce(0) { sum, cost in
+        if !isParentSame(parent, cost[0], cost[1]) {
+            parent = unionParent(parent, cost[0], cost[1])
+            return sum + cost[2]
+        } else {
+            return sum
+        }
     }
-    
-    func getParent(_ num: Int) -> Int {
-        if parent[num] == num { return num }
-        return getParent(parent[num])
-    }
-    
-    return costSum
 }
