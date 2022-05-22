2f60b250af367efb7823ee699745cf6f473b6ff5
BumgeunSong <isp1195@gmail.com>
2022-05-22 21:12:29 +0900

    Add dijkstra's algorithm

--- Algorithm_S3/Algorithm_S3/Dijkstras.swift
+++ Algorithm_S3/Algorithm_S3/Dijkstras.swift
@@ -20,6 +20,12 @@ struct Vertex<T> {
 }
 
 extension Vertex: Equatable where T: Equatable {}
+
+extension Vertex: Comparable where T: Comparable {
+    static func < (lhs: Vertex<T>, rhs: Vertex<T>) -> Bool {
+        lhs.data == rhs.data && lhs.index == rhs.index
+    }
+}
 extension Vertex: Hashable where T: Hashable {}
 
 extension Vertex: CustomStringConvertible {
@@ -96,11 +102,82 @@ enum Visit<T: Hashable> {
 }
 
 
-class Dijkstra<T: Hashable> {
+class Dijkstra<T: Hashable & Comparable> {
     typealias Graph = AdjacencyList<T>
     let graph: Graph
     
     init(graph: Graph) {
         self.graph = graph
     }
+    
+    func route(to destination: Vertex<T>, with paths: [Vertex<T>: Visit<T>]) -> [Edge<T>] {
+        // 도착지점에서 시작
+        var vertex = destination
+        var path: [Edge<T>] = []
+        
+        // 현재 노드의 Visit 정보를 가져옴. 만약 .edge라면 조건문을 실행.
+        // 현재 path에 이전 노드로 가는 edge를 추가
+        // 현재 노드를 엣지의 시작지점으로 이동시킴.
+        while let visit = paths[vertex], case .edge(let edge) = visit {
+            path = [edge] + path
+            vertex = edge.source
+        }
+        return path
+    }
+    
+    func distance(to destination: Vertex<T>, with paths: [Vertex<T>: Visit<T>]) -> Double {
+        let path = route(to: destination, with: paths)
+        let distances = path.compactMap { $0.weight }
+        return distances.reduce(0.0, +)
+    }
+    
+    func shortestPath(from start: Vertex<T>) -> [Vertex<T>: Visit<T>] {
+        // 시작 노드의 방문 상태를 '시작'으로 설정해서 저장.
+        var paths: [Vertex<T>: Visit<T>] = [start: .start]
+        
+        var priorityQueue = PriorityQueue<Vertex<T>>(sort: {
+            self.distance(to: $0, with: paths) < self.distance(to: $1, with: paths)
+        })
+        priorityQueue.enqueue(start)
+    
+        
+        while let vertex = priorityQueue.dequeue() {
+            // 현재 vertex의 모든 edge를 순회한다.
+            for edge in graph.edges(from: vertex) {
+                // edge에 weight가 있는지 확인.
+                guard edge.weight != nil else { continue }
+                
+                // 해당 엣지가 가리키는 꼭지점이 방문한 상태가 아닐때
+                // 혹은 해당 엣지의 웨이트가 기존보다 저렴할 때
+                // priority queue를 업데이트한다.
+                if paths[edge.destination] == nil || distance(to: vertex, with: paths) < distance(to: edge.destination, with: paths) {
+                    paths[edge.destination] = .edge(edge)
+                    priorityQueue.enqueue(edge.destination)
+                }
+            }
+        }
+        return paths
+    }
+    
+    func shortestPath(to destination: Vertex<T>, paths: [Vertex<T>: Visit<T>]) -> [Edge<T>] {
+        return route(to: destination, with: paths)
+    }
+    
+    func getAllShortestPath(from source: Vertex<T>) -> [Vertex<T>: [Edge<T>]] {
+        var pathDict = [Vertex<T>: [Edge<T>]]()
+        
+        let paths = shortestPath(from: source)
+        for vertex in graph.vertices {
+            pathDict[vertex] = shortestPath(to: source, paths: paths)
+        }
+        
+        return pathDict
+    }
 }
+
+/*
+ 다익스트라 알고리즘의 시간 복잡도
+ 
+ 
+ */
+
