5819ee76912f45dc925aa8db6788213dbd7eabdb
BumgeunSong <isp1195@gmail.com>
2022-08-05 20:19:41 +0900

    Add solution to combination Sum

--- Algorithm_S4/Algorithm_S4/combinationSum.swift
+++ Algorithm_S4/Algorithm_S4/combinationSum.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    
+    if target == 0 { return [[]] }
+    if target < 0 { return [] }
+    
+    var result = [[Int]]()
+    for (i, pick) in candidates.enumerated() {
+        let remain = Array(candidates[i..<candidates.count])
+        let comboRemain = combinationSum(remain, target - pick)
+        result += comboRemain.map { [pick] + $0 }
+    }
+    
+    return result
+}
